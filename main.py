8986694ecf53500200278454d97e962289cc0dce
gaurav-gaikwad-12071998 <122888092+gaurav-gaikwad-12071998@users.noreply.github.com>
2024-08-05 10:46:06 +0530

    created setup for scrapping data from movie page

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from imdb_urls import ExtractMoviesListFromIMDB
 
 
-if __name__ == '__main__':
-    extract =  ExtractMoviesListFromIMDB(2024)
+if __name__ == "__main__":
+    extract = ExtractMoviesListFromIMDB(1960)
     extract.run()
